2997b6b0da10033b6d210927953d1d47106f33a8
nakarou0404 <nakaroutibi@gmail.com>
2020-05-31 11:21:39 +0900

    KaihatutyuuHyouji

---
+++ Assets/Scripts/kaihatutyuu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class kaihatutyuu : MonoBehaviour
+{
+
+    public GameObject buttonMessage;
+    public GameObject ButtonMessageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PushButtonMemo() {
+        DisplayMessage("開発中");
+    }
+
+
+
+    void DisplayMessage(string mes) {
+        buttonMessage.SetActive(true);
+        ButtonMessageText.GetComponent<Text>().text = mes;
+    }
+
+
+    public void PushButtonMessage()
+    {
+        buttonMessage.SetActive(false);
+    }
+
+}
