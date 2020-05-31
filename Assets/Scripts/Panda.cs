109633efbf08cf2456a567fd0a5cb562ae5a57e8
nakarou0404 <nakaroutibi@gmail.com>
2020-05-31 15:00:11 +0900

    PandaRotate

---
+++ Assets/Scripts/Panda.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panda : MonoBehaviour
+{
+    public GameObject panda1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PushButtonPanda() {
+        panda1.transform.Rotate(new Vector3(0, 0, 5));
+    }
+}
